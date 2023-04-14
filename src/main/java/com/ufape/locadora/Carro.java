1ab589597bd5f15c39bcc0eb9204083c40e05809
SeveroGabriel <bielsevero@gmail.com>
2023-04-14 16:01:03 -0300

    Criada a fachada.

--- src/main/java/com/ufape/locadora/Carro.java
+++ src/main/java/com/ufape/locadora/Carro.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.Table;
+
 
 @Entity
+@Table(name = "carros")
 public class Carro {
 
     @Id
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  outputDir: 'src/main/resources/static',
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+}
